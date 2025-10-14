3ade85a215f7ca106f42d34ef2512c90571f4ef7
Abhishek A B <bsheks@gmail.com>
2025-10-15 01:11:14 +0530

    Made the add button functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import AddProjectPage from "./components/AddProjectPage";
 import BlankProjectWindow from "./components/BlankProjectWindow";
 import YourProjects from "./components/YourProjects";
+import {useState} from 'react';
 
 function App() {
-  let projectCount=0;
+
+  const [projectList, setProjectList] = useState(["Jalagaran Kayalli keysbeku", "Pavitra Gowda Jotheg Madve"]);
+
+  function handleAddProject(newItem){
+      setProjectList((prevValue) => 
+      [...prevValue, newItem]
+      );
+    }
+
   return (
     <div className="flex w-full min-h-screen">
       <div className="w-[30%]">
-        <YourProjects/>
+        <YourProjects projectList={projectList} handleAddProject={handleAddProject}/>
       </div>
       <div className="w-[70%]">
-        {projectCount>0?<AddProjectPage/>:<BlankProjectWindow/>}
+        {projectList.length>0?<AddProjectPage/>:<BlankProjectWindow/>}
       </div>
     </div>
   );
--- src/components/YourProjects.jsx
+++ src/components/YourProjects.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ProjectListItem from './ProjectListItem';
+import {useState} from 'react';
 
-export default function YourProjects(){
+export default function YourProjects({projectList, handleAddProject}){
 
     return(<div className="flex-col justify-start align-top bg-[#3E2723] h-full p-8 space-y-6">
         <p className='text-2xl font-serif text-white mb-4'>Your Projects</p>
         <div className='inline-block px-4 py-2 bg-orange-500 rounded-lg hover:bg-orange-600 transition-colors cursor-pointer'>
-            <button className='text-white font-medium'>+ Add Project</button>
+            <button className='text-white font-medium' onClick={() => handleAddProject("Jalagara")}>+ Add Project</button>
         </div>
             <ul>
-            <ProjectListItem project="Jalagaran Kayalli Keysodu" />
-            <ProjectListItem project="Prestige watchman jotheg horadodu" />
-            <ProjectListItem project="Pavithra Gowdan Jotheg Magu madkolodu" />
+            {projectList.map((projectName, index) => <ProjectListItem key={index} project={projectName} />)}
         </ul>
     </div>);
 }
